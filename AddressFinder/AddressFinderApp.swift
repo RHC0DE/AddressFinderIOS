e0075207d9aae4fdf379f9e694b7e35000cd7b91
R H <96940361+RHC0DE@users.noreply.github.com>
2022-07-05 21:53:28 +0200

    Initial Commit

---
+++ AddressFinder/AddressFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AddressFinderApp.swift
+//  AddressFinder
+//
+//  Created by Guest User on 05/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct AddressFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
